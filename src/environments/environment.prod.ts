e757c2e7712f4a8b31a4cccade94e568484b0959
bac-ta <bac93.it@gmail.com>
2020-09-26 23:57:34 +0700

    update source

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://172.22.0.5:1234/api'
+  apiUrl: 'http://172.22.0.5:1234/api',
+  BOSH_SERVICE: 'http://172.22.0.3/:7070/http-bind/',
+  DOMAIN: 'dimagesharevn.develop'
 };
